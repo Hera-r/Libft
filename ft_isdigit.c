8f7a1e3b14b448a8705736cd5d382dbcb19a9b91
Hera.r <tennyhera.r@gmail.com>
2022-10-17 14:41:16 +0200

    ajout fonction  ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrandria <hrandria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/17 10:57:42 by hrandria          #+#    #+#             */
-/*   Updated: 2022/10/17 14:30:04 by hrandria         ###   ########.fr       */
+/*   Created: 2022/10/17 14:30:19 by hrandria          #+#    #+#             */
+/*   Updated: 2022/10/17 14:40:32 by hrandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
+int	ft_isdigit(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
 }
@@ -24,8 +24,8 @@ int	ft_isalpha(int c)
 
 int main(void)
 {
-	printf("fake - : %d \n", ft_isalpha(123));
-	printf("original - : %d \n", isalpha(122));
+	printf("fake - : %d \n", ft_isdigit(58));
+	printf("original - : %d \n", isdigit(58));
 
 	return 0;
 }
