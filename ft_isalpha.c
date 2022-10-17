93e13834604bcf4a429823aa04bb0a040df49b1b
Hera.r <tennyhera.r@gmail.com>
2022-10-17 12:38:38 +0200

    ajout fonction ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,9 +1,32 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrandria <hrandria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 10:57:42 by hrandria          #+#    #+#             */
+/*   Updated: 2022/10/17 12:37:23 by hrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+int	ft_isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
+
+/*
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
-	write(1, "Hello", 5);
-	write(1, "\n", 1);
+	printf("fake - : %d \n", ft_isalpha(123));
+	printf("original - : %d \n", isalpha(122));
+
 	return 0;
 }
+*/
