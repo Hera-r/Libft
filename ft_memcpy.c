869ab4e407f641d61024620ae19d24e6d0309f94
Hera.r <tennyhera.r@gmail.com>
2022-10-29 15:47:33 +0200

    ajout fonction memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrandria <hrandria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 12:04:41 by hrandria          #+#    #+#             */
+/*   Updated: 2022/10/29 15:46:12 by hrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t count)
+{
+	int		i;
+	char	*new_dest;
+
+	i = 0;
+	new_dest = dest;
+	while (*(char *)src && count--)
+	{
+		new_dest[i++] = *(char *)src++;
+	}
+	new_dest[i] = '\0';
+	return (new_dest);
+}
+
+/*
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void)
+{
+   
+	const char f_src[50] = "https://www.apple.com/fr/macbook-air-m2/";
+	char f_dest[50];
+	strcpy(f_dest,"Heloooo!!");
+	printf("Before ft_memcpy dest = %s\n", f_dest);
+	ft_memcpy(f_dest, f_src, strlen(f_src)+1);
+	printf("After ft_memcpy dest = %s\n\n", f_dest);
+
+	printf("################### \n\n");
+	  
+   const char src[50] = "https://www.apple.com/fr/macbook-air-m2/";
+   char dest[50];
+   strcpy(dest,"Heloooo!!");
+   printf("Before memcpy dest = %s\n", dest);
+   memcpy(dest, src, strlen(src)+1);
+   printf("After memcpy dest = %s\n", dest);
+   
+
+   return(0);
+}*/
