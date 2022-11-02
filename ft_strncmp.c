6337e760210539f53e3dff6e35537f2bba567066
Hera.r <tennyhera.r@gmail.com>
2022-11-02 21:06:01 +0100

    ajout 3 nouveaux fonctions

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,8 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
-	while ((s1[i] == s2[i]) && n--)
-	i++;
+	while ((s1[i] == s2[i]) && n)
+   {
+      i++;
+      n--;
+   }
+
 	return (s1[i] - s2[i]);
 }
 
